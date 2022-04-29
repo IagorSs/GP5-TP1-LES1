496cdba0515b537629f58a99936d074e9ee97a47
Erick Henrique Dutra de Souza <55605746+ErickHDdS@users.noreply.github.com>
2022-04-28 22:42:55 -0300

    handling footer responsiveness

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -15,7 +15,6 @@ function Footer() {
         <div className="cefet_logo">
           <Link href="https://www.cefetmg.br/">
             <Avatar
-              style={{ margin: "10px" }}
               variant="square"
               sx={{ width: 315, height: 90 }}
               src="https://www.cefetmg.br/wp-content/uploads/2019/11/logo_topo.png"
@@ -23,15 +22,10 @@ function Footer() {
           </Link>
         </div>
 
-        <span className="infos">
-          Laboratório de Engenharia de Software I <br></br>
-          Copyright © {date()}
-        </span>
-
         <div className="authors">
           <Box
             className="authors_links"
-            sx={{ "& > :not(style)": { m: 3 } }}
+            sx={{ "& > :not(style)": { m: 2 } }}
             fontSize={14}
           >
             <a
@@ -39,7 +33,6 @@ function Footer() {
               href="https://www.linkedin.com/in/erickhenriquedds/"
             >
               <Avatar
-                sx={{ width: 50, height: 50 }}
                 style={{ margin: "10px" }}
                 src="https://github.com/ErickHDdS.png"
               />
@@ -51,7 +44,6 @@ function Footer() {
               href="https://www.linkedin.com/in/iagor-s-486635138/"
             >
               <Avatar
-                sx={{ width: 50, height: 50 }}
                 style={{ margin: "10px" }}
                 src="https://github.com/IagorSs.png"
               />
@@ -63,7 +55,6 @@ function Footer() {
               href="https://www.linkedin.com/in/vitor-laguardia-xavier-25200819b/"
             >
               <Avatar
-                sx={{ width: 50, height: 50 }}
                 style={{ margin: "10px" }}
                 src="https://github.com/IagorSs.png"
               />
@@ -75,7 +66,6 @@ function Footer() {
               href="https://www.linkedin.com/in/vitor-laguardia-xavier-25200819b/"
             >
               <Avatar
-                sx={{ width: 50, height: 50 }}
                 style={{ margin: "10px" }}
                 src="https://github.com/IagorSs.png"
               />
@@ -83,6 +73,11 @@ function Footer() {
             </a>
           </Box>
         </div>
+
+        <span className="infos">
+          Laboratório de Engenharia de Software I <br></br>
+          Copyright © {date()}
+        </span>
       </section>
     </footer>
   );
