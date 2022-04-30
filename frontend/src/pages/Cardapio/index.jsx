d8c518a3ac8b763c7f7509f0e75e5dc1f6de882d
Erick Henrique Dutra de Souza <55605746+ErickHDdS@users.noreply.github.com>
2022-04-30 19:19:12 -0300

    adding pizza cards on the menu page

--- frontend/src/pages/Cardapio/index.jsx
+++ frontend/src/pages/Cardapio/index.jsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import React from "react";
+import CardPizza from "../../components/CardPizza";
+import "./style.css";
 
 export default function Cardapio() {
-  return <h2>Cardapio</h2>;
+  let quantPizzaCards = 9;
+  let pizzasInfo = [];
+
+  async function handleGetPizzaDetail() {
+    let data;
+
+    for (var i = 0; i < quantPizzaCards; i++) {
+      // data = await getMovieDetail(moviesListOfGenre.at(initial).id);
+      data = {
+        link: "https://claudia.abril.com.br/wp-content/uploads/2020/07/pizza-pepperoni.jpg",
+      };
+      pizzasInfo.push(data);
+    }
+  }
+
+  handleGetPizzaDetail();
+
+  return (
+    <section className="main-cardapio">
+      <h1 className="cardapio-title">Cardápio</h1>
+
+      {pizzasInfo.length > 0 && (
+        <div className="list-pizzas">
+          {pizzasInfo.map((pizzaInfo) => (
+            <CardPizza pizza={pizzaInfo} />
+          ))}
+        </div>
+      )}
+    </section>
+  );
 }
