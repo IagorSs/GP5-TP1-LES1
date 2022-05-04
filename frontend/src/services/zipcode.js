c0668b58bea15e5a13c720768ca80448e919588d
Erick Henrique Dutra de Souza <55605746+ErickHDdS@users.noreply.github.com>
2022-05-04 15:02:42 -0300

    removing dead code

--- frontend/src/services/zipcode.js
+++ frontend/src/services/zipcode.js
@@ -1,35 +1,7 @@
 import axios from "axios";
 
-// const zipcode = axios.create({
-//   baseURL: "https://viacep.com.br/ws/",
-// });
-
 const zipcode = async (cep) => {
   return axios.get("https://viacep.com.br/ws/" + cep + "/json");
 };
 
-// const zipcode = async (cep) => {
-//   try {
-//     await axios.get("https://viacep.com.br/ws/" + cep + "/json");
-//     // console.log(resp.data);
-//   } catch (err) {
-//     // Handle Error Here
-//     console.error(err);
-//   }
-// };
-
 export default zipcode;
-
-// const axios = require("axios");
-
-// const zipcode = async (complement) => {
-//   try {
-//     await axios.get("https://viacep.com.br/ws/" + complement + "/json");
-//     // console.log(resp.data);
-//   } catch (err) {
-//     // Handle Error Here
-//     console.error(err);
-//   }
-// };
-
-// zipcode();
