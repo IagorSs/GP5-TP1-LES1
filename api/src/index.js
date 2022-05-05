f5cbba31f0ca44af02a9398576b6faa80a2d616f
Erick Henrique Dutra de Souza <55605746+ErickHDdS@users.noreply.github.com>
2022-05-04 21:40:26 -0300

    add cors

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import route from "./route/route.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -8,6 +9,14 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  app.use(cors());
+  next();
+});
+
 app.use(route);
 
 app.listen(port, () => {
