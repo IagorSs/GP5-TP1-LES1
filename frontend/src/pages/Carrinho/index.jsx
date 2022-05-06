94d1acc7f8825d8a281699862b57b1797bcae497
Erick Henrique Dutra de Souza <55605746+ErickHDdS@users.noreply.github.com>
2022-05-06 00:14:51 -0300

    adding automation in the order value field

--- frontend/src/pages/Carrinho/index.jsx
+++ frontend/src/pages/Carrinho/index.jsx
@@ -5,21 +5,30 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import ShoppingCartCheckoutIcon from "@mui/icons-material/ShoppingCartCheckout";
 import InputAdornment from "@mui/material/InputAdornment";
-
+import { convertToMoney } from "../../utils/string";
 import "./style.css";
 
 export default function Carrinho() {
   const [products, setProducts] = useState([]);
+  const [orderValue, setOrderValue] = useState(0);
 
+  async function handleSetValue() {
+    let value = 0;
+    products.map((product) => {
+      value += product.Price;
+    });
+    setOrderValue(value);
+  }
   useEffect(() => {
     const fetchProducts = async () => {
       let productsStorage = JSON.parse(localStorage.getItem("cart"));
-
+      console.log(productsStorage);
       setProducts(productsStorage);
+      handleSetValue();
     };
 
     fetchProducts();
-  }, []);
+  });
 
   return (
     <section className="main-cart">
@@ -40,10 +49,11 @@ export default function Carrinho() {
           <TextField
             id="value-read-only-input"
             label="Valor do pedido (R$)"
+            value={convertToMoney(orderValue)}
             InputProps={{
               readOnly: true,
               startAdornment: (
-                <InputAdornment position="start">R$</InputAdornment>
+                <InputAdornment position="start"></InputAdornment>
               ),
             }}
             variant="standard"
