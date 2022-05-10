e14570b5e6344137446bbe75a1f55c01bff46f0a
IagorSs <iagor.penaranda@uol.com.br>
2022-05-10 09:03:39 -0300

    Create methods to set api token header

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -4,4 +4,12 @@ const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_ENDPOINT,
 });
 
+export const setApiToken = (token) => {
+  axiosInstance.defaults.headers.token = token;
+}
+
+export const removeApiToken = () => {
+  delete axiosInstance.defaults.headers.token;
+}
+
 export default axiosInstance;
